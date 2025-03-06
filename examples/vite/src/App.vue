e471c9c9019dbcd4153b770412d4498d485e46eb
erkelost <1256029807@qq.com>
2025-03-06 21:21:49 +0800

    fix: createVNode error

--- examples/vite/src/App.vue
+++ examples/vite/src/App.vue
@@ -1,32 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+
+const count = ref(0)
+function counter() {
+  count.value++
+  console.log("count1", count.value);
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/aaa.jpg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>Im testing</div>
+  <button type="button" @click="counter">count1 is {{ count }}</button>
+  <HelloWorld />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
