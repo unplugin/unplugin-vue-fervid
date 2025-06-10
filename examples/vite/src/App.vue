6149d4c64f79ab83e3ca7f4c882c8318ba647b8e
erkelost <1256029807@qq.com>
2025-06-10 11:23:26 +0800

    chore: update fervid version

--- examples/vite/src/App.vue
+++ examples/vite/src/App.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-
-const count = ref(0)
-function counter() {
-  count.value++
-  console.log("count1", count.value);
-}
 </script>
 
 <template>
-  <div>Im testing</div>
-  <img src="./assets/aaa.jpg" width="100" height="100" alt="logo" />
-  <button type="button" @click="counter">count1 is {{ count }}</button>
-  <HelloWorld />
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
